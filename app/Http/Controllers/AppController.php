6ef8c146239d78a30301c2190604838e374299f0
Krzysztof Wieczorek <krzysiu_1996@o2.pl>
2020-09-25 19:55:47 +0200

    change rdf route method to POST

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -36,7 +36,7 @@ class AppController extends Controller
         ]);
 
         $response_data = collect($response->json())->unique('URI')->map(function($item) {
-            $item['types'] = explode(',', $item['types']);
+            $item['types'] = $item['types']!=''?explode(',', $item['types']):[];
             return $item;
         });
 
